d75f5fd7277108ee7ca2c96b8f472e8e24fcf6d5
EthanRajkumar <144576900+EthanRajkumar@users.noreply.github.com>
2023-10-23 20:47:45 -0400

    Responsiveness Update
    Added media queries for responsive design, cleaned up CSS classes, and added contacts section.

--- main.js
+++ main.js
@@ -1,14 +1,2 @@
-let learnMoreButton = document.getElementById("learn-more");
-let downArrow = document.querySelector("down-arrow");
 
-let rotateArrow = function() {
-    learnMoreButton.querySelector("p").innerHTML = "WERWER";
-    downArrow.style.rotate = "0deg";
-}
-
-let revertArrow = function() {
-    downArrow.style.rotate = "-90deg";
-}
-
-learnMoreButton.addEventListener("mouseenter", rotateArrow);
-learnMoreButton.addEventListener('mouseleave', revertArrow);
+document.getElementById("bg-video").playbackRate = 0.125;
