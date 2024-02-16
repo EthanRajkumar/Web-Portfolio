1fa844dc812123927fb94e808083b3691e2f1579
EthanRajkumar <144576900+EthanRajkumar@users.noreply.github.com>
2024-02-16 00:09:05 -0500

    Spring 2024 Update
    Added projects section, cleaned up visual design, and added JavaScript code.

--- main.js
+++ main.js
@@ -1,2 +1,35 @@
+let current_index = 0;
 
-document.getElementById("bg-video").playbackRate = 0.125;
+function incrementSlide() {
+    let slideshow_div = document.getElementById('slideshow-gallery');
+    let info_div = document.getElementById('slideshow-info');
+    let slideshow_length = slideshow_div.getElementsByClassName('slide').length;
+    slideshow_div.getElementsByClassName('slide')[current_index].classList.add('hidden');
+    info_div.getElementsByClassName('info-slide')[current_index].classList.add('hidden');
+
+    current_index++;
+
+    if(current_index == slideshow_length)  {
+        current_index = 0;
+    }
+
+    slideshow_div.getElementsByClassName('slide')[current_index].classList.remove('hidden');
+    info_div.getElementsByClassName('info-slide')[current_index].classList.remove('hidden');
+}
+
+function decrementSlide() {
+    let slideshow_div = document.getElementById('slideshow-gallery');
+    let info_div = document.getElementById('slideshow-info');
+    let slideshow_length = slideshow_div.getElementsByClassName('slide').length;
+    slideshow_div.getElementsByClassName('slide')[current_index].classList.add('hidden');
+    info_div.getElementsByClassName('info-slide')[current_index].classList.add('hidden');
+
+    current_index--;
+
+    if(current_index < 0)  {
+        current_index = slideshow_length - 1;
+    }
+
+    slideshow_div.getElementsByClassName('slide')[current_index].classList.remove('hidden');
+    info_div.getElementsByClassName('info-slide')[current_index].classList.remove('hidden');
+}
